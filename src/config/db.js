567d7cced7e20a2944855ebd99d6811932fb67b7
Nelson Tommogo <nelsontommogo9@gmail.com>
2025-06-23 13:03:01 +0300

    Av backend

--- src/config/db.js
+++ src/config/db.js
@@ -1,17 +1,16 @@
-// config/db.js
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
 const connectDB = async () => {
-try {
+  try {
     await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
-    console.log('✅ MongoDB Connected');
-} catch (error) {
-    console.error('❌ MongoDB Connection Failed:', error.message);
+    console.log("✅ MongoDB Connected");
+  } catch (err) {
+    console.error("❌ MongoDB connection error:", err.message);
     process.exit(1);
-}
+  }
 };
 
-module.exports = connectDB;
+export default connectDB;
